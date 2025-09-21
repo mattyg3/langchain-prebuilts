12b4c521ed8b53fa26f9de60ea90b2e4c84e8d6d
mattyg3 <surfff4life@gmail.com>
2025-09-21 12:05:34 -0700

    debugging save_run func

--- src/langchain-prebuilts/example_creative_writer_team.py
+++ src/langchain-prebuilts/example_creative_writer_team.py
@@ -1,7 +1,7 @@
 import llm_init # Needed to track traces with langsmith
 import workflows.creative_writing_room.compile_workflow 
 from workflows.creative_writing_room.compile_workflow import app, create_agent_state  #AgentState,
-from util_funcs import save_graph_viz, get_plot, parse_plot_doc #, format_feedback
+from util_funcs import get_plot, parse_plot_doc, save_run #, format_feedback save_graph_viz, 
 
 # ---- Interactive Session ----
 if __name__ == "__main__":
@@ -12,8 +12,11 @@ if __name__ == "__main__":
     # Initialize an empty state
     state = create_agent_state(optional_human_nodes=False, context=f'{str(plot_doc)}\n')
 
-    # Save graph diagram to folder
-    save_graph_viz(app) #file_name='saved_langgraph_viz'
+    
+    # save_graph_viz(app) #file_name='saved_langgraph_viz'
 
     # Initial full pipeline run
     state = app.invoke(state) #saves state to file_name='saved_state_'+timestamp
+
+    # Save graph diagram to folder
+    save_run(state, app, run_name='dev_run1', save_path='src/langchain-prebuilts/outputs')
--- src/langchain-prebuilts/util_funcs.py
+++ src/langchain-prebuilts/util_funcs.py
@@ -1,3 +1,4 @@
+
 ##PRINT FUNCTIONS##
 def format_feedback(raw_text: str) -> str:
     """
@@ -46,11 +47,11 @@ import json
 from pathlib import Path
 from datetime import datetime
 def save_graph_viz(graph, file_name='saved_langgraph_viz', parent_path='src/langchain-prebuilts/outputs'):
-    #Set PATH for graphviz
+    # Set PATH for graphviz
     import os
     os.environ["PATH"] += os.pathsep + r"C:\Program Files\Graphviz\bin"
 
-    #Save Path
+    # Save Path
     save_path = Path(f'{parent_path}/{file_name}_{datetime.now().strftime("%Y%m%d_%H%M%S")}.json')
     save_path.parent.mkdir(parents=True, exist_ok=True)
 
@@ -70,13 +71,65 @@ def save_graph_viz(graph, file_name='saved_langgraph_viz', parent_path='src/lang
 
 
 def save_graph_state(state, file_name='saved_state', parent_path='src/langchain-prebuilts/outputs'):
-    #Save Path
+    # Save Path
     save_path = Path(f'{parent_path}/{file_name}_{datetime.now().strftime("%Y%m%d_%H%M%S")}.json')
     save_path.parent.mkdir(parents=True, exist_ok=True)
 
     with open(save_path, "w") as f:
         json.dump(state, f, indent=4)
 
+def save_agent_outputs(state, file_name='saved_summary_doc', parent_path='src/langchain-prebuilts/outputs'):
+    # Save Path
+    save_path = Path(f'{parent_path}/{file_name}_{datetime.now().strftime("%Y%m%d_%H%M%S")}.md')
+    save_path.parent.mkdir(parents=True, exist_ok=True)
+
+    # Combine Agents outputs
+    # content = f"OUTPUT\n\n{state['head_writer_outputs'][-1]}\n\n{state['world_outputs'][-1]}\n\n{state['character_outputs'][-1]}\n\n{state['plot_outputs'][-1]}\n\n"
+    content = f"OUTPUT\n\n{state['head_writer_outputs']}\n\n{state['world_outputs']}\n\n{state['character_outputs']}\n\n{state['plot_outputs']}\n\n"
+
+    with open(save_path, "w", encoding="utf-8") as f: #"a" for append
+        f.write(content)
+
+def create_unique_folder(base_name: str, parent: str = ".") -> Path:
+    """
+    Create a folder with base_name inside parent directory.
+    If it exists, create a variant like base_name_1, base_name_2, etc.
+    Returns the final Path.
+    """
+    parent_path = Path(parent)
+    folder = parent_path / base_name
+    counter = 1
+
+    while folder.exists():
+        folder = parent_path / f"{base_name}_REV{counter}"
+        counter += 1
+
+    folder.mkdir(parents=True)
+    return folder
+
+def save_run(state, graph, run_name='unnamed_run', save_path='src/langchain-prebuilts/outputs'):
+    # #Set PATH for graphviz
+    # import os
+    # os.environ["PATH"] += os.pathsep + r"C:\Program Files\Graphviz\bin"
+
+    # Create Output Folder if none exists
+    Path(f'{save_path}/{run_name}').parent.mkdir(parents=True, exist_ok=True)
+
+    # Create Run Folder, If already exists, create a REV
+    run_folder = create_unique_folder(base_name=run_name, parent=save_path)
+
+    # Save Graph State
+    save_graph_state(state, parent_path=run_folder)
+
+    # Save Graph Visual
+    save_graph_state(graph, parent_path=run_folder)
+
+    # # Save Agent Output Doc
+    # save_agent_outputs(state, parent_path=run_folder)
+
+    print(f"\n\n    ..Saved to: {run_folder}")
+
+    
 
 
 ##WRITING PERSONALITIES##
